34b7f31aaf18f57fd48544f812f1ba30603918df
ProvLeon <providence.leonard@gmail.com>
2024-01-04 23:19:41 +0000

    updated

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,26 +15,21 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_left_node;
+	binary_tree_t *new;
 
 	if (parent == NULL)
-	{
 		return (NULL);
-	}
 
-	new_left_node = binary_tree_node(parent, value);
-
-	if (new_left_node == NULL)
-	{
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
 		return (NULL);
-	}
 
 	if (parent->left != NULL)
 	{
-		new_left_node->left = parent->left;
-		parent->left->parent = new_left_node;
+		new->left = parent->left;
+		parent->left->parent = new;
 	}
+	parent->left = new;
 
-	parent->left = new_left_node;
-	return (new_left_node);
+	return (new);
 }
